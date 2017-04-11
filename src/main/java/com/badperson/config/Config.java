fb1b953e6ff538ed8677d1c7180ce363602aefb0
yutuer <yutuerzf@163.com>
2017-04-11 22:47:58 +0800

    add excel

--- src/main/java/com/badperson/config/Config.java
+++ src/main/java/com/badperson/config/Config.java
@@ -1,9 +1,11 @@
 package com.badperson.config;
 
 public class Config {
+	public static final String CONF_PREFIX = "";
+//	public static final String CONF_PREFIX = "conf/";
 	
-	public static final String PROP_FILE = "port.properties";
-	public static final String EXCEL_DIR = "excel/";
+	public static final String PROP_FILE = CONF_PREFIX + "port.properties";
+	public static final String EXCEL_DIR = CONF_PREFIX + "excel/";
 	public static String FILE_PATH = "G:/miniTool/";
 
 	public static final String ENCODING = "GBK";
--- src/main/java/com/badperson/config/MysqlConfig.java
+++ src/main/java/com/badperson/config/MysqlConfig.java
@@ -4,7 +4,7 @@ public class MysqlConfig {
 
 	public static final String OUT_DIR = "mysql/";
 	public static final String OUT_FILENAME = "connections.ncx";
-	public static final String TEMPLATE_FILENAME = "mysqlModel.txt";
+	public static final String TEMPLATE_FILENAME = Config.CONF_PREFIX + "mysqlModel.txt";
 
 	public static final String FirstLine = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n";
 	public static final String SecondLine = "<Connections Ver=\"1.1\">\r\n";
